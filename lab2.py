5e8044a4671730f777e100f28e20f1135cf2ca67
seanm44 <C00310393@setu.ie>
2025-09-22 11:44:28 +0000

    update

--- lab2.py
+++ lab2.py
@@ -1,4 +1,16 @@
+import re
+
 with open("auth.log" , "r") as f:
-    pattern = r"\d+\.\d+\.\d+\.\d+"
     for line in f:
-       print(f:.findall(pattern, line))
+        print(line)
+
+ips = [] 
+pattern = r"\d+\.\d+\.\d+\.\d+"
+found_ips = re.findall(pattern, line)
+for ip in found_ips:
+        ips.append(ip)
+
+unique_ips = set(ips)
+print("Unique IPs:")
+for ip in unique_ips:
+    print(ip)
